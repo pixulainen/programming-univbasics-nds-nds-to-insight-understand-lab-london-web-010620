11bb7b4a5a48c36f5844bd5f3fc5f75ca5ef2601
George Apetrei <george.apetrei@gmail.com>
2019-12-21 00:04:55 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,7 +14,7 @@ def print_first_directors_movie_titles
   spielberg_movies =  directors_database[0][:movies]
   index =0 
   while index < spielberg_movies[index] do 
-    
-  
-
+    puts spielberg_movies[index][:title]
+    index +=1 
+  end
 end 
